3bb86815297698285d3948dbe9ae9956f4b727b6
Waqas <wmunir232@gmail.com>
2023-11-07 17:03:50 +0500

    Worked With Heap Memory

--- Programming in C/pointers.c
+++ Programming in C/pointers.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <conio.h>
+#include <stdlib.h>
 
 int main()
 {
@@ -12,4 +13,10 @@ int main()
     printf("%d\n", p);  // returns value stored pointer (same as address of a)
     printf("%d", *p);   // also known as defrencing
     printf("\n%u", &p); // returns address of pointer
+
+    // * Working With Heap
+    int *p1;
+    // malloc() returns pointer of type void so we have to explicitly convert it into data type of pointer
+    p1 = (int *)malloc(5 * sizeof(int)); // ? Created Array of size 5 in heap and accessed it using pointer p1
+    printf("\n%d", p1);
 }
