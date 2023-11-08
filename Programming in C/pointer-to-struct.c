d9fc3e6519a5268399e71499f0605747cbc25948
Waqas <wmunir232@gmail.com>
2023-11-08 18:05:45 +0500

    Allocated Heap memory to structure

--- Programming in C/pointer-to-struct.c
+++ Programming in C/pointer-to-struct.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <conio.h>
 
@@ -11,6 +12,10 @@ int main()
 {
     struct rectangle r = {10, 6};
     struct rectangle *p = &r;
+
+    // ? Making structure in heap
+    struct rectangle *p1;
+    p1 = (struct rectangle *)malloc(sizeof(struct rectangle));
 
     p->breadth = 20;
     printf("Area of rectangle is %d", p->length * p->breadth);
