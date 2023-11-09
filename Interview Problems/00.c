bbb3329dc74aeef972e0bb5ec1a3c5b881015df6
Waqas <wmunir232@gmail.com>
2023-11-09 19:23:42 +0500

    1st Interview Question

---
+++ Interview Problems/00.c
@@ -0,0 +1,12 @@
+// ! Question ==>  What is the value of x
+// ? Answer ==> printf always returns length of string which is gonna print
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int x = 0;
+    x = printf("\nHello");
+    printf("\n%d", x);
+}
