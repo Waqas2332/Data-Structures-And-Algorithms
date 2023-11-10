32ca180257837f0788a1b5559f5b370ac146f5fd
Waqas <wmunir232@gmail.com>
2023-11-10 10:49:06 +0500

    Written Menu Driven Program For Array

--- Arrays/array-adt.c
+++ Arrays/array-adt.c
@@ -18,6 +18,8 @@ void initialize(struct Array *a)
 
 void display(struct Array *a)
 {
+    printf("\nSize of Array is %d", a->size);
+    printf("\nLength of Array is %d", a->length);
     printf("\nElements in Array are ...\n");
     for (int i = 0; i < a->length; i++)
     {
@@ -26,9 +28,49 @@ void display(struct Array *a)
     printf("\n\nEnd Of Array\n");
 }
 
+void insertOne(struct Array *a, int value)
+{
+    if (a->length == a->size)
+    {
+        int *p = (int *)malloc((a->size * 2) * sizeof(int));
+        for (int i = 0; i < a->length; i++)
+        {
+            p[i] = a->A[i];
+        }
+        a->A = p;
+        a->size = a->size * 2;
+    }
+    a->A[a->length] = value;
+    a->length++;
+}
+
 int main()
 {
     struct Array a;
+    int menu;       // For prompting Menu
+    int value;      // For inserting value
+    int isTrue = 1; // While loop control
     initialize(&a);
-    display(&a);
+
+    // Logic For Menu display
+    while (isTrue)
+    {
+        printf("\nEnter 1 For Displaying Array, 2 For Inserting Element in array, 0 for closing the program : ");
+        scanf("%d", &menu);
+        if (menu == 0)
+        {
+            break;
+        }
+        if (menu == 1)
+        {
+            display(&a);
+            continue;
+        }
+        if (menu == 2)
+        {
+            printf("\nEnter Value to insert in Array ");
+            scanf("%d", &value);
+            insertOne(&a, value);
+        }
+    }
 }
