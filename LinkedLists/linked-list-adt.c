5fc949e43bf49ef37833a3ef1c9dea06867fe1bf
Waqas <wmunir232@gmail.com>
2023-12-03 13:22:05 +0500

    Written Menu driven program for linked list

--- LinkedLists/linked-list-adt.c
+++ LinkedLists/linked-list-adt.c
@@ -11,4 +11,22 @@ int main()
 {
     struct Node *p = (struct Node *)malloc(sizeof(struct Node));
     p->data = 10;
+    int menu;       // For prompting Menu
+    int value;      // For inserting value
+    int isTrue = 1; // While loop control
+
+    // Logic For Menu display
+    while (isTrue)
+    {
+        printf("\nEnter 1 For Displaying Linked List, 0 for closing the program : ");
+        scanf("%d", &menu);
+        if (menu == 0)
+        {
+            break;
+        }
+        if (menu == 1)
+        {
+            continue;
+        }
+    }
 }
