918ee398d199c456645f4eab24d63b65938f19a0
Waqas <wmunir232@gmail.com>
2023-12-04 16:23:40 +0500

    Implement Searching

--- LinkedLists/linked-list-adt.c
+++ LinkedLists/linked-list-adt.c
@@ -53,6 +53,23 @@ void display()
     printf("\nNumber of elements in linked lists : %d\nSum of all elements in linked list : %d\nMaximum element in linked list : %d\nMinimum Element in Linked List : %d", count, sum, max, min);
 }
 
+void searchNode(int value)
+{
+    struct Node *temp = first;
+    int index = 0;
+    while (temp != NULL)
+    {
+        if (temp->data == value)
+        {
+            printf("%d is found at %d index", temp->data, index);
+            return;
+        }
+        index++;
+        temp = temp->next;
+    }
+    printf("%d is not found in Linked List", value);
+}
+
 int main()
 {
     int menu;       // For prompting Menu
@@ -61,7 +78,7 @@ int main()
     // Logic For Menu display
     while (isTrue)
     {
-        printf("\nEnter 1 For Displaying info about Linked List,2 for Inserting Element in Linked List, 0 for closing the program : ");
+        printf("\nEnter 1 For Displaying info about Linked List,\n2 for Inserting Element in Linked List,\nEnter 3 for Searching in Linked List\n ,0 for closing the program : ");
         scanf("%d", &menu);
         if (menu == 0)
         {
@@ -76,6 +93,14 @@ int main()
         {
             printf("\nEnter the value to be inserted in linked list : ");
             insertNode();
+            continue;
+        }
+        if (menu == 3)
+        {
+            int value = 0;
+            printf("\nEnter the value to for searching in linked list : ");
+            scanf("%d", &value);
+            searchNode(value);
             continue;
         }
     }
