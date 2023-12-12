ac6dcea5b057306868f5e7009f1b374a72b4b0bc
Waqas <wmunir232@gmail.com>
2023-12-12 19:34:05 +0500

    Done Array Question

---
+++ LeetCode Problems/Arrays/18_max-subarray.c
@@ -0,0 +1,21 @@
+int maxSubArray(int *nums, int numsSize)
+{
+    int currSum = 0;
+    int maxSum = nums[0];
+
+    for (int i = 0; i < numsSize; i++)
+    {
+        currSum += nums[i];
+
+        if (currSum > maxSum)
+        {
+            maxSum = currSum;
+        }
+
+        if (currSum < 0)
+        {
+            currSum = 0;
+        }
+    }
+    return maxSum;
+}
